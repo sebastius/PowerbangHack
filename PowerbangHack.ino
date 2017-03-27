703d10a515a86a2f575e7b3500a9027d274d407c
Sebastius <bas@knutselaar.net>
2017-03-27 20:08:11 +0200

    First commit!
    Not tested!

---
+++ PowerbangHack.ino
@@ -0,0 +1,33 @@
+// Powerbang HACK
+// https://revspace.nl/Powerbank
+// https://muxtronics.nl/powerbank.html
+//
+// For ATTiny85
+// 
+// Sebastius Revspace
+
+// To get an AT Tiny programmed use https://github.com/damellis/attiny
+
+#include <TinyWireM.h> // https://github.com/adafruit/TinyWireM
+#include <avr/sleep.h>
+
+#define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
+
+void setup() {
+  TinyWireM.begin(); 
+  adc_disable();
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); 
+}
+
+void sleep1s() {
+  sleep_enable();                   
+  sleep_cpu();                       
+}
+
+void loop() {
+  TinyWireM.beginTransmission(0x6B);
+  TinyWireM.write(0x02);
+  TinyWireM.write(0x9C);
+  TinyWireM.endTransmission();
+  sleep1s();
+}
