685f2b919baac2ee0cd37825a476431f3dee3c63
Sebastius <sebastianoort@MacBook-Pro-van-Sebastian.local>
2017-04-08 10:48:21 +0200

    20hrs charging
    Default is 12hrs

--- PowerbangHack.ino
+++ PowerbangHack.ino
@@ -3,7 +3,7 @@
 // https://muxtronics.nl/powerbank.html
 //
 // For ATTiny85
-// 
+//
 // Sebastius Revspace
 
 // To get an AT Tiny programmed use https://github.com/damellis/attiny
@@ -14,20 +14,26 @@
 #define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
 
 void setup() {
-  TinyWireM.begin(); 
+  TinyWireM.begin();
   adc_disable();
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); 
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 }
 
 void sleep1s() {
-  sleep_enable();                   
-  sleep_cpu();                       
+  sleep_enable();
+  sleep_cpu();
 }
 
 void loop() {
   TinyWireM.beginTransmission(0x6B);
-  TinyWireM.write(0x02);
-  TinyWireM.write(0xBC);
+  TinyWireM.write(0x02); // REG02
+  TinyWireM.write(0xBC); // 3A charging
   TinyWireM.endTransmission();
+
+  TinyWireM.beginTransmission(0x6B);
+  TinyWireM.write(0x05); // REG05
+  TinyWireM.write(0b10011110); // 20hrs charging
+  TinyWireM.endTransmission();
+  
   sleep1s();
 }
